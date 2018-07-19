f6a6fe8ed29463b588a0c8d1082628d7c308ead2
xld1393679430 <1393679430@qq.com>
2018-07-19 11:09:41 +0800

    add swiper

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import router from './router'
 //移动端click延迟300ms的问题
 import fastClick from "fastclick"
 
+//引入vue-awesome-swiper轮播图
+import VueAwesomeSwiper from "vue-awesome-swiper"
+import "swiper/dist/css/swiper.css"
+
+
 //引入iconfont.css图标库
 import "styles/iconfont.css" /*-----------这里能用styles路径的原因是在webpack.base,conf.js设置了alias（30line）----------*/
 
@@ -19,7 +24,7 @@ import "styles/border.css"
 Vue.config.productionTip = false;
 
 fastClick.attach(document.body);
-
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/Home/components/Swiper.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item,id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+      <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
+      <!--<div class="swiper-button-next" slot="button-next"></div>-->
+      <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default{
+      name:"HomerSwiper",
+      data(){
+          return{
+            swiperOption:{
+              pagination:".swiper-pagination",
+              loop:true,
+//              autoplay:true,
+//              speed:2000
+            },
+            swiperList:[
+              {
+                  id:"001",
+                  imgUrl:"http://img1.qunarzz.com/piao/fusion/1806/f5/ea83238744f31102.jpg_750x200_73b8477c.jpg"
+              },
+              {
+                id:"002",
+                imgUrl:"http://img1.qunarzz.com/piao/fusion/1806/3c/c72a1ccd4d7b2202.jpg_750x200_b88bbab4.jpg"
+              },
+            ]
+          }
+      }
+  }
+</script>
+
+<style lang="stylus" scoped="scoped">
+  /*----样式穿透--*/
+  .wrapper  >>>  .swiper-pagination-bullet-active
+    background :#fff
+  .wrapper
+    width:100%
+    height:0
+    overflow :hidden
+    padding-bottom 26.67%
+    background :#eee
+    .swiper-img
+      width:100%
+</style>
